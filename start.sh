ad441e1e9325307203a189dbd01e6aef14e081db
swatiyad <swati.nadcabtechnology.com>
2022-11-07 16:58:21 +0530

    stakes menu, color theme, all issues reolved except

--- start.sh
+++ start.sh
@@ -5,9 +5,9 @@ export ETHEREUM_JSONRPC_VARIANT=parity
 export ETHEREUM_JSONRPC_HTTP_URL=https://rpc01.taralityscan.com/
 export ETHEREUM_JSONRPC_WS_URL=ws://rpc01.taralityscan.com/ws
 export DATABASE_URL=postgres://taralscan:tarality@localhost:5432/blockscout
-export ETHEREUM_JSONRPC_TRACE_URL=https://rpc01.taralityscan.com/
-export NETWORK=Taral
-export SUBNETWORK="Taral"
+export ETHEREUM_JSONRPC_TRACE_URL=https://rpc01.taralityscan.com
+export NETWORK=Tarality Scan
+export SUBNETWORK="Testnet"
 export LOGO=/images/taral-logo.png
 export LOGO_FOOTER=/images/taral-logo.png
 export ETHEREUM_JSONRPC_TRANSPORT=http
@@ -15,13 +15,13 @@ export NETWORK_PATH=/
 export API_PATH=/
 export BLOCKSCOUT_HOST=testnet.taralityscan.com
 export BLOCKSCOUT_PROTOCOL=https
-export SECRET_KEY_BASE=BASE=kJXNW97h8Mt5IDAe/TatOro5x74HkrXoxodPh/K0+c2QxlpH6v/g+b63F/LwCyxJ
+export SECRET_KEY_BASE=e+E4TnErUa0Fme4COz2IboFKyLP/uO5H/oyB1vRncV+8WgHhYVCd98iAsf6UWss+
 # export CHECK_ORIGIN=false
 # export PORT=4000
-export COIN=Ana
-export COIN_NAME=Ana
-export BLOCKSCOUT_VERSION=v4.1.5-beta
-export RELEASE_LINK=https://github.com/blockscout/blockscout/releases/tag/v4.1.5-beta
+export COIN=Taral
+export COIN_NAME=Taral
+export BLOCKSCOUT_VERSION=v4.1.3-beta
+export RELEASE_LINK=https://github.com/blockscout/blockscout/releases/tag/v4.1.3-beta
 
 export LINK_TO_OTHER_EXPLORERS=false
 export DISABLE_EXCHANGE_RATES=true
@@ -29,53 +29,43 @@ export ENABLE_TXS_STATS=true
 export SHOW_PRICE_CHART=true
 export SHOW_TXS_CHART=true
 # export EXCHANGE_RATES_COINGECKO_COIN_ID=tarality
-# export EXCHANGE_RATES_SOURCE=coin_market_cap
-# export EXCHANGE_RATES_COINGECKO_API_KEY=4529b9d5-bfe7-4403-8259-a599c76b1ec5
+# export EXCHANGE_RATES_SOURCE=coin_gecko
+# export EXCHANGE_RATES_COINGECKO_API_KEY=
 # export COINGECKO_COIN_ID=tarality
-export CACHE_TXS_COUNT_PERIOD=7200
-export CACHE_BLOCK_COUNT_PERIOD=7200
-export HISTORY_FETCH_INTERVAL=240
-export TXS_HISTORIAN_INIT_LAG=0
-export TXS_STATS_DAYS_TO_COMPILE_AT_INIT=10
-export COIN_BALANCE_HISTORY_DAYS=90
-export APPS_MENU=false
-export TOKEN_METADATA_UPDATE_INTERVAL=1800
-export CACHE_TOKEN_EXCHANGE_RATE_PERIOD=3600000
+
+export APPS_MENU=true
 # export EXTERNAL_APPS='[{ "title": "Ana Bridge", "url": "https://bridge.inrx.io/" } ]'
-export ENABLE_SOURCIFY_INTEGRATION=true 
-export SOURCIFY_SERVER_URL=https://sourcify.dev/server
-export SOURCIFY_REPO_URL=https://repo.sourcify.dev/contracts/
+export ENABLE_SOURCIFY_INTEGRATION=true
 export CHAIN_ID=4369
 export DISPLAY_TOKEN_ICONS=true
 export RE_CAPTCHA_SECRET_KEY=6Ldgd1ofAAAAACmME9HbA3tJnISuXIUSC-D-dz1g
 export RE_CAPTCHA_CLIENT_KEY=6Ldgd1ofAAAAAA59_hntvL0AW6IdSMGPgpWl22_b
-export JSON_RPC=https://rpc01.taralityscan.com/
+export JSON_RPC=https://rpc01.taralityscan.com
 export ADMIN_PANEL_ENABLED=true
 # export METADATA_CONTRACT=
-export VALIDATORS_CONTRACT=0x874967F5Fe0b75Ee6592D981E5E47a2D11E3E2Bf
-export POS_STAKING_CONTRACT=0xBb1cB38f61777bdFd9dfcB84e559E592D8809fDA
-export RANDOM_AURA_CONTRACT=0xF494A55fB0f347E8526f13e5337C9EAfF273E873
+# export VALIDATORS_CONTRACT=0x0a8b1423Ee7E313E98d5159e994bed5cBE7Dc0D8
+export POS_STAKING_CONTRACT=0xd671f4Dab6eaf47C34CA71a3B43769960D2E7F32
 export ENABLE_POS_STAKING_IN_MENU=true
+export CHAIN_SPEC_PATH=https://gist.githubusercontent.com/amankumarp/c4a09c1d6a81ede18270f76ccc395ab8/raw/48068b42519ba7851a6f007a7bc04ebe0b894def/spec.json
+# export SUPPLY_MODULE=TokenBridge
+# export SOURCE_MODULE=coin_gecko
+#export GAS_PRICE =1
+export CACHE_TOTAL_GAS_USAGE_PERIOD=7200
+export CACHE_ADDRESS_TRANSACTIONS_GAS_USAGE_COUNTER_PERIOD=14400
 export TOKEN_BALANCE_ON_DEMAND_FETCHER_THRESHOLD_MINUTES=60
 export GAS_PRICE_ORACLE_NUM_OF_BLOCKS=200
 export GAS_PRICE_ORACLE_SAFELOW_PERCENTILE=35
 export GAS_PRICE_ORACLE_AVERAGE_PERCENTILE=60
-export GAS_PRICE_ORACLE_FAST_PERCENTILE=90
-export GAS_PRICE_ORACLE_CACHE_PERIOD=300
-export DISPLAY_TOKEN_ICONS=true
-export CHAIN_SPEC_PATH=https://gist.githubusercontent.com/amankumarp/4042810bc109983b42e746f7d23389d4/raw/907d8c5866a0557c9390efd8ddb9df4c8df804b4/spec.json
-# export SUPPLY_MODULE=TokenBridge
-# export SOURCE_MODULE=coin_gecko
+export SHOW_TENDERLY_LINK=true
 export API_RATE_LIMIT=100
 export API_RATE_LIMIT_BY_KEY=30
 export API_RATE_LIMIT_BY_IP=30
 export INDEXER_MEMORY_LIMIT=7
 
 
-/bin/mix ecto.drop
-
-/bin/mix ecto.create 
-/bin/mix ecto.migrate
+# /bin/mix ecto.drop
+# /bin/mix ecto.create 
+# /bin/mix ecto.migrate
 /bin/mix phx.digest.clean 
 /bin/mix phx.digest
 /bin/mix phx.server
